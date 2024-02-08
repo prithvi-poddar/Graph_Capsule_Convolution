4f4fa78542f519e1c5a79011be7bcd50cf1eaf21
prithvi-poddar <prithvi.poddar@gmail.com>
2024-02-08 15:34:49 -0500

    GCN_CAPS first commit

---
+++ networks.py
@@ -0,0 +1,108 @@
+import torch 
+import torch.nn as nn
+from torch import Tensor
+from torch.nn import Parameter
+import torch.functional as F
+from typing import List
+from torch_geometric.utils import get_laplacian
+
+class Laplacian_GCN_parallel(nn.Module):
+    # TODO: check the backpropogation
+    def __init__(self, input_dim, output_dim, k, activation=nn.ReLU(), device=torch.device("cuda" if torch.cuda.is_available() else "cpu")):
+        super(Laplacian_GCN_parallel, self).__init__()
+        self.__in_dim = input_dim
+        self.__out_dim = output_dim
+        self.__k = k
+        self.act = activation
+        self.weights = nn.ModuleList([nn.Linear(self.__in_dim, self.__out_dim) for i in range(self.__k+1)])
+        self.device = device
+        self.to(device)
+
+    def conv(self, X, L, W, k):
+        return W(torch.matmul(torch.linalg.matrix_power(L,k),X))
+    
+    def forward(self, data=None, X=None, L=None):
+        if X == None:
+            X = data.x
+            L = get_laplacian(data.edge_index)
+        futures : List[torch.jit.Future[torch.Tensor]] = []
+        for k in range(self.__k+1):
+            futures.append(torch.jit.fork(self.conv, X, L, self.weights[k], k))
+        
+        results = torch.empty((self.__k+1, X.shape[0], self.__out_dim))
+        for idx, future in enumerate(futures):
+            results[idx,:,:] = torch.jit.wait(future)
+        X = torch.sum(results, dim=0)
+        return self.act(X)
+    
+class Laplacian_GCN(nn.Module):
+    def __init__(self, input_dim, output_dim, k, activation=nn.ReLU(), device=torch.device("cuda" if torch.cuda.is_available() else "cpu")):
+        super(Laplacian_GCN, self).__init__()
+        self.__in_dim = input_dim
+        self.__out_dim = output_dim
+        self.__k = k
+        self.act = activation
+        self.weights = nn.ModuleList([nn.Linear(self.__in_dim, self.__out_dim) for i in range(self.__k+1)])
+        self.device = device
+        self.to(device)
+    
+    def forward(self, data=None, X=None, L=None):
+        if X == None:
+            X = data.x
+            L = get_laplacian(data.edge_index)
+        X_sum = torch.zeros((X.shape[0], self.__out_dim), device=self.device, requires_grad=True)
+        for k in range(self.__k+1):
+            X_sum = X_sum + self.weights[k](torch.matmul(torch.linalg.matrix_power(L,k),X))
+        return self.act(X_sum)   
+    
+class Primary_Capsule(nn.Module):
+    #TODO: Do we take the statistical moments in the first layer?
+    def __init__(self, input_dim, output_dim, gcn_model='Laplacian', p=3, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"), **kwargs):
+        super(Primary_Capsule, self).__init__()
+        self.__in_dim = input_dim
+        self.__out_dim = output_dim
+        self.p = p
+        self.activation = kwargs['activation']
+        if gcn_model == 'Laplacian':
+            k = kwargs['k']
+            activation = kwargs['activation']
+            self.nets = nn.ModuleList([Laplacian_GCN(input_dim=self.__in_dim, output_dim=self.__out_dim, k=k, activation=activation) for i in range(self.p)])
+        self.device = device
+
+    def forward(self, data=None, X=None, L=None):
+        if X == None:
+            X = data.x
+            L = get_laplacian(data.edge_index)
+        outs = []
+        for idx, net in enumerate(self.nets):
+            outs.append(net(X=X, L=L))
+        output = torch.stack(outs, -1)
+        output = torch.flatten(output, -2, -1)
+        return self.activation(output)
+    
+class Secondary_Capsule(nn.Module):
+    def __init__(self, input_dim, output_dim, gcn_model='Laplacian', p=3, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"), **kwargs):
+        super(Secondary_Capsule, self).__init__()
+        self.__in_dim = input_dim
+        self.__out_dim = output_dim
+        self.p = p
+        self.activation = kwargs['activation']
+        if gcn_model == 'Laplacian':
+            k = kwargs['k']
+            activation = kwargs['activation']
+            self.nets = nn.ModuleList([Laplacian_GCN(input_dim=self.__in_dim, output_dim=self.__out_dim, k=k, activation=activation) for i in range(self.p)])
+        self.device = device
+        
+    def forward(self, data=None, X=None, L=None):
+        if X == None:
+            X = data.x
+            L = get_laplacian(data.edge_index)
+        X_moments = X*X
+        for _ in range(self.p-2):
+            X_moments = X_moments*X
+        outs = []
+        for idx, net in enumerate(self.nets):
+            outs.append(net(X=X_moments, L=L))
+        output = torch.stack(outs, -1)
+        output = torch.flatten(output, -2, -1)
+        return self.activation(output)

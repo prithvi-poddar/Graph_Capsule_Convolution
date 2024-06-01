40c088f3982b796d54b613c71893d9cf1c903fea
Prithvi Dibyendu Poddar <prithvid@albatross.eng.buffalo.edu>
2024-06-01 14:36:30 -0400

    decoder added

---
+++ Models/ONR/decoder.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from Networks.networks import *
+from Networks.utils import *
+
+class MHA_Decoder(nn.Module):
+    def __init__(self,
+                 context_dim = 128,
+                 key_dim = 256,
+                 value_dim = 256,
+                 num_heads = 8,
+                 hidden_dim = [64, 64],
+                 out_feats = 50,
+                 device=torch.device("cuda" if torch.cuda.is_available() else "cpu")):
+        super(MHA_Decoder, self).__init__()
+        self.mha = nn.MultiheadAttention(embed_dim=context_dim,
+                                         num_heads=num_heads,
+                                         dropout=0.2,
+                                         kdim=key_dim,
+                                         vdim=value_dim)
+        self.batchnorm1 = nn.BatchNorm1d(num_features=context_dim, affine=False)
+        self.linear1 = nn.Linear(in_features=context_dim,
+                                 out_features=hidden_dim[0])
+        self.linear2 = nn.Linear(in_features=hidden_dim[0],
+                                 out_features=hidden_dim[1])
+        self.batchnorm2 = nn.BatchNorm1d(num_features=hidden_dim[1], affine=False)
+        self.linear3 = nn.Linear(in_features=hidden_dim[1],
+                                 out_features=out_feats)
+        self.activation = nn.ReLU()
+        self.softmax = nn.Softmax(dim=-1)
+        self.device = device
+        self.to(device)
+
+    def forward(self, context, key, value):
+        x, _ = self.mha(context, key, value, need_weights=False)
+        # x = self.batchnorm1(x)
+        x = self.activation(self.linear1(x))
+        x = self.activation(self.linear2(x))
+        # x = self.batchnorm2(x)
+        probs = self.softmax(self.linear3(x))
+        return probs

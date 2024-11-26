b1635cb8497486ce03665b330d3e97427bbb365d
Prithvi Dibyendu Poddar <prithvid@albatross.eng.buffalo.edu>
2024-11-26 14:30:33 -0500

    added moments in first caps

--- Networks/networks.py
+++ Networks/networks.py
@@ -42,7 +42,7 @@ class Laplacian_GCN(nn.Module):
         self.__out_dim = output_dim
         self.__k = k
         self.act = activation
-        self.weights = nn.ModuleList([nn.Linear(self.__in_dim, self.__out_dim) for i in range(self.__k+1)])
+        self.weights = nn.ModuleList([nn.Linear(self.__in_dim, self.__out_dim, bias=False) for i in range(self.__k+1)])
         for idx, net in enumerate(self.weights):
             nn.init.xavier_uniform_(net.weight.data)
         self.device = device
@@ -93,25 +93,25 @@ class Edge_Laplacian_GCN(nn.Module):
         return X_final  
     
 class Primary_Capsule(nn.Module):
-    #TODO: Do we take the statistical moments in the first layer?
-    def __init__(self, input_dim, output_dim, gcn_model='Edge_Laplacian', p=3, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"), **kwargs):
+    def __init__(self, input_dim, output_dim, latent_dim=64, gcn_model='Laplacian', p=3, k=3, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"), **kwargs):
         super(Primary_Capsule, self).__init__()
         self.__in_dim = input_dim
         self.__out_dim = output_dim
         self.p = p
+        self.k = k
         self.activation = kwargs['activation']
         self.device = device
+        self.linear = nn.Linear(in_features=self.__in_dim, out_features=latent_dim)
         if gcn_model == 'Laplacian':
-            k = kwargs['k']
             activation = kwargs['activation']
-            self.nets = nn.ModuleList([Laplacian_GCN(input_dim=self.__in_dim, output_dim=self.__out_dim, k=k, activation=activation, device=self.device) for i in range(self.p)])
+            self.nets = nn.ModuleList([Laplacian_GCN(input_dim=latent_dim, output_dim=self.__out_dim, k=self.k, activation=activation, device=self.device) for i in range(self.p)])
         elif gcn_model == 'Edge_Laplacian':
-            k = kwargs['k']
             activation = kwargs['activation']
             edge_feat_dim = kwargs['edge_feat_dim']
-            self.nets = nn.ModuleList([Edge_Laplacian_GCN(input_dim=self.__in_dim, edge_input_dim=edge_feat_dim, output_dim=self.__out_dim, k=k, activation=activation, device=self.device) for i in range(self.p)])
+            self.nets = nn.ModuleList([Edge_Laplacian_GCN(input_dim=latent_dim, edge_input_dim=edge_feat_dim, output_dim=self.__out_dim, k=self.k, activation=activation, device=self.device) for i in range(self.p)])
         # self.batchnorm = nn.BatchNorm1d(num_features=self.__out_dim*self.p)
         self.batchnorm = nn.BatchNorm1d(num_features=100)
+        self.to(device)
         
 
     def forward(self, data=None, X=None, L=None):
@@ -119,6 +119,8 @@ class Primary_Capsule(nn.Module):
             X = data.x
             L = get_laplacian(data.edge_index)
         outs = []
+        X = self.linear(X)
+        X = X**self.p
         for idx, net in enumerate(self.nets):
             outs.append(net(X=X, L=L))
         output = torch.stack(outs, -1)
@@ -126,22 +128,23 @@ class Primary_Capsule(nn.Module):
         return self.activation(output)
     
 class Secondary_Capsule(nn.Module):
-    def __init__(self, input_dim, output_dim, gcn_model='Edge_Laplacian', p=3, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"), **kwargs):
+    def __init__(self, input_dim, output_dim, gcn_model='Edge_Laplacian', p=3, k=3, device=torch.device("cuda" if torch.cuda.is_available() else "cpu"), **kwargs):
         super(Secondary_Capsule, self).__init__()
         self.__in_dim = input_dim
         self.__out_dim = output_dim
         self.p = p
+        self.k = k
         self.activation = kwargs['activation']
         self.device = device
         if gcn_model == 'Laplacian':
-            k = kwargs['k']
+            # k = kwargs['k']
             activation = kwargs['activation']
-            self.nets = nn.ModuleList([Laplacian_GCN(input_dim=self.__in_dim, output_dim=self.__out_dim, k=k, activation=activation, device=self.device) for i in range(self.p)])
+            self.nets = nn.ModuleList([Laplacian_GCN(input_dim=self.__in_dim, output_dim=self.__out_dim, k=self.k, activation=activation, device=self.device) for i in range(self.p)])
         elif gcn_model == 'Edge_Laplacian':
-            k = kwargs['k']
+            # k = kwargs['k']
             activation = kwargs['activation']
             edge_feat_dim = kwargs['edge_feat_dim']
-            self.nets = nn.ModuleList([Edge_Laplacian_GCN(input_dim=self.__in_dim, edge_input_dim=edge_feat_dim, output_dim=self.__out_dim, k=k, activation=activation, device=self.device) for i in range(self.p)])
+            self.nets = nn.ModuleList([Edge_Laplacian_GCN(input_dim=self.__in_dim, edge_input_dim=edge_feat_dim, output_dim=self.__out_dim, k=self.k, activation=activation, device=self.device) for i in range(self.p)])
         # self.batchnorm = nn.BatchNorm1d(num_features=self.__out_dim*self.p)
         self.batchnorm = nn.BatchNorm1d(num_features=100)
         
@@ -150,9 +153,10 @@ class Secondary_Capsule(nn.Module):
         if X == None:
             X = data.x
             L = get_laplacian(data.edge_index)
-        X_moments = X*X
-        for _ in range(self.p-2):
-            X_moments = X_moments*X
+        # X_moments = X*X
+        # for _ in range(self.p-2):
+        #     X_moments = X_moments*X
+        X_moments = X**self.p
         outs = []
         for idx, net in enumerate(self.nets):
             outs.append(net(X=X_moments, L=L))
